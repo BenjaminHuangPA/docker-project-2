db526b203334f2225484ba32f282b86f258c04fc
BenjaminHuangPA <huan1398@purdue.edu>
2021-09-03 20:20:08 -0700

    Testing CI/CD

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, world! This app was built by Ben on 9/2/21!! And here are some changes that were added after the app was run for the first time. Pretty neat, huh?'
+    return 'Hello, world! This app was built by Ben on 9/2/21!! The following changes were made for testing CI/CD.'
